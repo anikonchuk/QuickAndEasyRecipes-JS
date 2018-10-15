b6f6e9b217a7bbab423e83d6940c5b243e29e3c4
anikonchuk <anikonch@gmail.com>
2018-10-15 13:28:22 -0400

    Create nested resources for Recipes inside Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:new, :create] do
+    resources :recipes [:index, :new, :create]
+  end
   resources :recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
