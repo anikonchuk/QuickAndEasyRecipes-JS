1fc9c47ed12c24e2c18694803dba0c6837a2df6c
anikonchuk <anikonch@gmail.com>
2018-10-15 13:35:32 -0400

    Generate controllers for Welcome and Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post '/logout' => 'sessions#destroy'
 
   resources :users, only: [:new, :create] do
-    resources :recipes [:index, :new, :create]
+    resources :recipes, only: [:index, :new, :create]
   end
   resources :recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
