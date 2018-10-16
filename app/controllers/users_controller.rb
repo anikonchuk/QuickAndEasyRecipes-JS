19bc4fe11354a75b560aeadbf84b2efdbaff02c2
anikonchuk <anikonch@gmail.com>
2018-10-16 09:57:09 -0400

    Build create action in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_recipes(@user)
+    else
+      render :new
+    end
   end
 
   private
