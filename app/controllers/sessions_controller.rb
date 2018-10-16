d4d3b0c7cf0626ecc4cdc7b5acbcb168f3ec0ec3
anikonchuk <anikonch@gmail.com>
2018-10-16 11:06:10 -0400

    Add create route to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_recipes_path(@user)
+    else
+      render :new
+    end    
   end
 
   def destroy
     session.clear
     redirect_to root_path
   end
+
 end
