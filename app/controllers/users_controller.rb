39706ab480837cdaf5374d661a28704326137ad3
anikonchuk <anikonch@gmail.com>
2018-10-16 10:10:18 -0400

    Stub out recipes index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_recipes(@user)
+      redirect_to user_recipes_path(@user)
     else
       render :new
     end
