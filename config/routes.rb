03e5d91ea4d413fb9b326787725f81843ede0bdc
anikonchuk <anikonch@gmail.com>
2018-10-16 13:43:36 -0400

    Remove non-nested new and create routes for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create] do
     resources :recipes, only: [:index, :new, :create]
   end
-  resources :recipes
+  resources :recipes, except: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
