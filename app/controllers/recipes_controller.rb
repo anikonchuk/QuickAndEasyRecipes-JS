429c67d36898fc65a02488bfec60091dc2584dd8
anikonchuk <anikonch@gmail.com>
2018-10-16 10:54:42 -0400

    Add require_login before_action to recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,15 @@
 class RecipesController < ApplicationController
 
+  before_action :require_login
+
   def index
     @recipes = Recipe.all
   end
-  
+
+  private
+
+  def require_login
+    redirect_to root_path unless session.include? :user_id
+  end
+
 end
