6de59a4b4bb0f5aaf17a65123fd81a2294ea10ba
anikonchuk <anikonch@gmail.com>
2018-10-16 11:20:26 -0400

    Add nested index action to RecipesController

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,17 @@ class RecipesController < ApplicationController
   before_action :require_login
 
   def index
-    @recipes = Recipe.all
+    if params[:user_id]
+      @user = User.find_by(id: params[:user_id])
+      if @user
+        @recipes = @user.recipes
+      else
+        flash[:alert] = "User not found."
+        redirect_to recipes_path
+      end
+    else
+      @recipes = Recipe.all
+    end
   end
 
   private
