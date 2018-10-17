810b0f1b1baeabab40a800a4a74f125ec2248d16
anikonchuk <anikonch@gmail.com>
2018-10-17 16:13:06 -0400

    Add before_action and index and show actions to IngredientsController

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,13 @@
 class IngredientsController < ApplicationController
+
+  before_action :require_login
+
+  def index
+    @ingredients = Ingredient.by_name
+  end
+
+  def show
+    @ingredient = Ingredient.find_by(id: params[:id])
+  end
+
 end
