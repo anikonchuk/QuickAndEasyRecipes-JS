cc549a9aecb8a3a505df53277ab92f83b5655816
anikonchuk <anikonch@gmail.com>
2018-10-18 15:06:04 -0400

    Add by_name scope method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
 
   validates :username, presence: true
   validates :username, uniqueness: { case_sensitive: false }
+
+  scope :by_name, -> { order(name: :asc) }
 end
