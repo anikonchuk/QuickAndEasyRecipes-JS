d3720cffd6dd4da9217b2e41c6b5bd1369aec193
anikonchuk <anikonch@gmail.com>
2018-10-18 09:42:49 -0400

    Change SessionsController to deal with form_tag vs form_for in login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,16 @@
 class SessionsController < ApplicationController
 
   def new
-    @user = User.new
   end
 
   def create
-    @user = User.find_by(username: params[:user][:username])
-    if @user && @user.authenticate(params[:user][:password])
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to user_recipes_path(@user)
     else
       render :new
-    end    
+    end
   end
 
   def destroy
