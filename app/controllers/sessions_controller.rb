0144e09a0af8f4621ff067eb5b059b269f251824
anikonchuk <anikonch@gmail.com>
2018-10-20 13:17:45 -0400

    Change username to email in SessionsController and UsersController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to user_recipes_path(@user)
