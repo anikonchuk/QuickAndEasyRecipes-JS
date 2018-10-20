7eb1d9f77308cc01b9f437195638207e7adeb4cb
anikonchuk <anikonch@gmail.com>
2018-10-20 15:22:23 -0400

    Add find_or_create_by_omniauth class method to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :email, uniqueness: { case_sensitive: false }
 
+  def self.find_or_create_by_omniauth(auth_hash)
+    oauth_email = auth_hash["info"]["email"]
+    self.where(email: oauth_email).first_or_create do |user|
+      user.password = SecureRandom.hex
+    end
+  end
+
 end
