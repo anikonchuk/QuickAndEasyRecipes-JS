a3f593cf482c2e9a92aa0d85d84c61dcd923efcc
anikonchuk <anikonch@gmail.com>
2018-10-20 13:13:58 -0400

    Remove username and add email to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_16_131105) do
+ActiveRecord::Schema.define(version: 2018_10_20_171321) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -36,10 +36,10 @@ ActiveRecord::Schema.define(version: 2018_10_16_131105) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
