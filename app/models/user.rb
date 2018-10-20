743d90ec623c6018dda5b14eafd2aa1e73f93f3a
anikonchuk <anikonch@gmail.com>
2018-10-20 13:15:26 -0400

    Change validation in user model and change application layout to email vs username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :recipes
   has_secure_password
 
-  validates :username, presence: true
-  validates :username, uniqueness: { case_sensitive: false }
+  validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
 
 end
