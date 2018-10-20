6f88fe630e763046ff14466b38f9756ea4888346
anikonchuk <anikonch@gmail.com>
2018-10-20 16:14:42 -0400

    Change redirect path on login/user creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,12 @@ class SessionsController < ApplicationController
     if auth_hash = request.env["omniauth.auth"]
       @user = User.find_or_create_by_omniauth(auth_hash)
       session[:user_id] = @user.id
-      redirect_to user_recipes_path(@user)
+      redirect_to recipes_path
     else
       @user = User.find_by(email: params[:email])
       if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
-        redirect_to user_recipes_path(@user)
+        redirect_to recipes_path
       else
         render :new
       end
