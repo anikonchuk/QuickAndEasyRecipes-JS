9e2ec4fce55b02a91af260ef22baf2f00977475a
anikonchuk <anikonch@gmail.com>
2018-12-12 09:55:08 -0500

    Change redirect_to landing_path for Users and Sessions controllers on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,12 @@ class SessionsController < ApplicationController
     if auth_hash = request.env["omniauth.auth"]
       @user = User.find_or_create_by_omniauth(auth_hash)
       session[:user_id] = @user.id
-      redirect_to recipes_path
+      redirect_to landing_path
     else
       @user = User.find_by(email: params[:email])
       if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
-        redirect_to recipes_path
+        redirect_to landing_path
       else
         flash[:alert] = "Something went wrong. Please try again."
         redirect_to login_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to recipes_path
+      redirect_to landing_path
     else
       render :new
     end
