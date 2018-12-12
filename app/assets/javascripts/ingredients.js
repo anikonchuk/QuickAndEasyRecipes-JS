5a540706ac841dee4404a5b79a501a0081bf0bed
anikonchuk <anikonch@gmail.com>
2018-12-12 16:54:02 -0500

    Build out fetchAllIngredientData()

--- app/assets/javascripts/ingredients.js
+++ app/assets/javascripts/ingredients.js
@@ -7,6 +7,21 @@ function addListenerToIngredientsLink() {
   const ingredientsLink = document.getElementById("ingredients-link");
   ingredientsLink.addEventListener('click', function(e){
     e.preventDefault();
+    fetchAllIngredientData();
+  });
+}
+
+function fetchAllIngredientData() {
+  fetch('/ingredients.json')
+  .then(res => res.json())
+  .then(function(resp){
+    let htmlResponse = `<h2>All Ingredients</h2><p>Click on an ingredient to see recipes that use it!</p></ul>`;
+    resp.forEach(function(ingredient){
+      htmlResponse += `<li><a href="ingredients/${ingredient.id}" class="individual-ingredient-links">${ingredient.name}</a></li>`;
+    });
+    htmlResponse += `</ul>`
+    document.getElementById("landing-content").innerHTML = htmlResponse;
+    addListenerToIngredientNameLink();
   });
 }
 
