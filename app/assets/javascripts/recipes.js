30bb8ba76110886591f8c96f9f9e3c607ba5397b
anikonchuk <anikonch@gmail.com>
2018-12-12 10:04:20 -0500

    Add listener to all recipes link

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,3 +1,12 @@
 $(function() {
   console.log("JS is working");
+  addListenerToAllRecipesLink();
 })
+
+function addListenerToAllRecipesLink () {
+  const allRecipesLink = document.getElementById("all-recipes-link");
+  allRecipesLink.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log("All Recipes Link is Working");
+  });
+}
