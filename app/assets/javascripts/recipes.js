15ab9098a8890706aba7c2c28a89073b0306631d
anikonchuk <anikonch@gmail.com>
2018-12-13 16:47:47 -0500

    Use Recipe JS class in fetchUserRecipeData

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -90,7 +90,8 @@ function fetchUserRecipeData(url) {
   .then(resp => {
     let htmlResponse = `<h2>Your Recipes</h2><table><thead><th>Recipe Name</th><th>Time</th><th>Ingredients</th></thead><tbody>`;
     resp.forEach(recipe => {
-      htmlResponse += `<tr><td><a href="/recipes/${recipe.id}" class="recipe-name">${recipe.name}</a></td><td>${recipe.time} minutes</td><td>${recipe.ingredients.length} ingredients</td></tr>`;
+      let newRecipe = new Recipe(recipe);
+      htmlResponse += newRecipe.createRecipeTableRow();
     });
     htmlResponse += `</tbody></table>`
     document.getElementById("landing-content").innerHTML = htmlResponse;
