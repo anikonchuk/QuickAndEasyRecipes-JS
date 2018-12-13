0931a76f5f89a077bf0abdd21dbf7d57aa0af8a4
anikonchuk <anikonch@gmail.com>
2018-12-13 16:40:18 -0500

    Add arrow functions to fetchAllRecipeData

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -35,9 +35,9 @@ function addListenerToAllRecipesLink () {
 function fetchAllRecipeData() {
   fetch('/recipes.json')
   .then(res => res.json())
-  .then(function(resp){
+  .then(resp => {
     let htmlResponse = `<h2>All Recipes</h2><table><thead><th>Recipe Name</th><th>Time</th><th>Ingredients</th></thead><tbody>`;
-    resp.forEach(function(recipe){
+    resp.forEach(recipe => {
       htmlResponse += `<tr><td><a href="/recipes/${recipe.id}" class="recipe-name">${recipe.name}</a></td><td>${recipe.time} minutes</td><td>${recipe.ingredients.length} ingredients</td></tr>`;
     });
     htmlResponse += `</tbody></table>`
