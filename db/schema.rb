38b6bf55c8442430c2c9badf7689ecc1831ebb43
anikonchuk <anikonch@gmail.com>
2019-02-19 13:15:45 -0500

    run migrations and create new schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_20_171321) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
